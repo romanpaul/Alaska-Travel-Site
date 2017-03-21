4ee11c2636db115616b72a0b6a523bc0bef5a46c
Roman Paul <Roman@Embers-MacBook-Air.local>
2017-03-20 21:52:21 -0400

    Lightbox
    Lightbox has been added and is fully functional.

---
+++ JS/lightbox.js
@@ -0,0 +1,45 @@
+// Create a lightbox
+
+(function() {
+  var $lightbox = $("<div class='lightbox'></div>");
+  var $img = $("<img>");
+  var $caption = $("<p class='caption'></p>");
+
+// Hide the lightbox
+    
+    $lightbox.hide();
+    
+// Add image and caption to lightbox
+
+  $lightbox
+    .append($img)
+    .append($caption);
+
+// Add lighbox to document
+
+  $('body').append($lightbox);
+
+  $('.lightbox-gallery img').click(function(e) {
+    e.preventDefault();
+
+// Get image link and description
+    var src = $(this).attr("src");
+    var cap = $(this).attr("alt");
+
+// Add data to lighbox
+
+    $img.attr('src', src);
+    $caption.text(cap);
+
+// Show lightbox
+
+    $lightbox.fadeIn('fast');
+      
+// Exit the lightbox when the div is clicked anywhere
+
+    $lightbox.click(function() {
+      $lightbox.fadeOut('fast');
+    });
+  });
+
+}());
